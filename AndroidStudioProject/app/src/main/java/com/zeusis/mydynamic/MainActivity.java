c6bf649a60a4d20f1aba06397274dd39d98ce5db
rick.xue <2005xwf@163.com>
2016-07-25 15:11:48 +0800

    test

--- AndroidStudioProject/app/src/main/java/com/zeusis/mydynamic/MainActivity.java
+++ AndroidStudioProject/app/src/main/java/com/zeusis/mydynamic/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private int addResult;
     private JavaNative mJavaNative;
     private TextView mResultText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
